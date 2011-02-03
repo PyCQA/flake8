5a1a4917e1b94819f5e79e171916240f11aec602
Tarek Ziade <tarek@ziade.org>
2011-02-03 16:56:16 +0100

    bypass non-python files

--- flake8/__init__.py
+++ flake8/__init__.py
@@ -116,6 +116,8 @@ def hg_hook(ui, repo, **kwargs):
     files = []
     for rev in xrange(repo[kwargs['node']], len(repo)):
         for file_ in repo[rev].files():
+            if not file_.endswith('.py'):
+                continue
             if file_ not in files:
                 files.append(file_)
 

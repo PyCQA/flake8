b97f9f542d00cd1e1c40407f897094315ad874e9
Tarek Ziade <tarek@ziade.org>
2012-02-21 10:37:12 +0100

    io under py3

--- flake8/tests/test_mccabe.py
+++ flake8/tests/test_mccabe.py
@@ -1,6 +1,9 @@
 import unittest
 import sys
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 from flake8.mccabe import get_code_complexity
 

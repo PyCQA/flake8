b8aab10d5f7c6d07390f8e89e70e76135f40978d
Florent Xicluna <florent.xicluna@gmail.com>
2013-02-20 22:50:12 +0100

    setup.py: add classifiers for Python 2/3 compatibility

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development",
         "Topic :: Utilities",
     ],

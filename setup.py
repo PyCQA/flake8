20b49ee2578f338e441e9d9227cb740ff6d4b8db
Florent Xicluna <florent.xicluna@gmail.com>
2013-02-20 22:30:24 +0100

    setup.py: reorder entries

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license="MIT",
     version=__version__,
     description="code checking using pep8 and pyflakes",
+    long_description=README,
     author="Tarek Ziade",
     author_email="tarek@ziade.org",
     maintainer="Ian Cordasco",
@@ -22,15 +23,6 @@ setup(
         "pyflakes==0.6.1",
         "pep8==1.4.2",
     ],
-    long_description=README,
-    classifiers=[
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Utilities",
-    ],
     entry_points={
         'distutils.commands': ['flake8 = flake8.main:Flake8Command'],
         'console_scripts': ['flake8 = flake8.main:main'],
@@ -39,6 +31,14 @@ setup(
             'C90 = flake8.mccabe:McCabeChecker',
         ],
     },
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
     tests_require=['nose'],
     test_suite='nose.collector',
 )

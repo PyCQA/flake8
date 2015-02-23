bc18d8e2d1cb621ceaa554fe8ca64a720009d880
Ian Cordasco <graffatcolmingov@gmail.com>
2015-02-22 22:37:13 -0600

    Use the default ReadTheDocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ exclude_patterns = ['_build']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+#html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

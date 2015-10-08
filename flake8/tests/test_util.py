d42232554895b473a612e72cc11b68720f672bf2
Ian Cordasco <graffatcolmingov@gmail.com>
2015-10-07 22:09:04 -0500

    Rewrite some tests from !41

--- flake8/tests/test_util.py
+++ flake8/tests/test_util.py
@@ -1,58 +1,67 @@
 import unittest
-try:
-    from unittest import mock
-except ImportError:
-    import mock  # < PY33
 
 from flake8.util import option_normalizer
 
+
 class TestOptionSerializer(unittest.TestCase):
 
-    def test_true(self):
+    def test_1_is_true(self):
         option = option_normalizer('1')
-        self.assertEqual(option, True)
+        self.assertTrue(option)
 
+    def test_T_is_true(self):
         option = option_normalizer('T')
-        self.assertEqual(option, True)
+        self.assertTrue(option)
 
+    def test_TRUE_is_true(self):
         option = option_normalizer('TRUE')
-        self.assertEqual(option, True)
+        self.assertTrue(option, True)
 
+    def test_ON_is_true(self):
         option = option_normalizer('ON')
-        self.assertEqual(option, True)
+        self.assertTrue(option)
 
+    def test_t_is_true(self):
         option = option_normalizer('t')
-        self.assertEqual(option, True)
+        self.assertTrue(option)
 
+    def test_true_is_true(self):
         option = option_normalizer('true')
-        self.assertEqual(option, True)
+        self.assertTrue(option)
 
+    def test_on_is_true(self):
         option = option_normalizer('on')
-        self.assertEqual(option, True)
+        self.assertTrue(option)
 
-    def test_false(self):
+    def test_0_is_false(self):
         option = option_normalizer('0')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
+    def test_F_is_false(self):
         option = option_normalizer('F')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
+    def test_FALSE_is_false(self):
         option = option_normalizer('FALSE')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
+    def test_OFF_is_false(self):
         option = option_normalizer('OFF')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
+    def test_f_is_false(self):
         option = option_normalizer('f')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
+    def test_false_is_false(self):
         option = option_normalizer('false')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
+    def test_off_is_false(self):
         option = option_normalizer('off')
-        self.assertEqual(option, False)
+        self.assertFalse(option)
 
-    def test_multiple_option(self):
+    def test_parses_lists(self):
         answer = ['F401', 'F402', 'F403', 'F404']
 
         option = option_normalizer('F401,F402,F403,F404')

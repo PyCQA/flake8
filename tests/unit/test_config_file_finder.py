dd46f02b5863fb0850d3421b8cd698d1d3ecdfba
Ian Cordasco <graffatcolmingov@gmail.com>
2016-01-09 23:02:20 -0600

    Add config file fixtures
    Add more ConfigFileFinder tests

--- tests/unit/test_config_file_finder.py
+++ tests/unit/test_config_file_finder.py
@@ -1,5 +1,8 @@
 """Tests for the ConfigFileFinder."""
 import os
+import sys
+
+import mock
 
 from flake8.options import config
 
@@ -7,5 +10,19 @@ from flake8.options import config
 def test_uses_default_args():
     """Show that we default the args value."""
     finder = config.ConfigFileFinder('flake8', None, [])
-    assert finder.args == ['.']
     assert finder.parent == os.path.abspath('.')
+
+
+@mock.patch.object(sys, 'platform', 'win32')
+def test_windows_detection():
+    """Verify we detect Windows to the best of our knowledge."""
+    finder = config.ConfigFileFinder('flake8', None, [])
+    assert finder.is_windows is True
+
+
+def test_cli_config():
+    cli_filepath = 'tests/fixtures/config_files/cli-specified.ini'
+    finder = config.ConfigFileFinder('flake8', None, [])
+
+    parsed_config = finder.cli_config(cli_filepath)
+    assert parsed_config.has_section('flake8')

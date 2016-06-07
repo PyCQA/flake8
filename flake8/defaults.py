3d512ae2f585b8c97f88299b0b4e44df89514751
Ian Cordasco <graffatcolmingov@gmail.com>
2016-06-07 18:03:37 -0500

    Update default ignore for when we upgrade pep8

--- flake8/defaults.py
+++ flake8/defaults.py
@@ -1,7 +1,7 @@
 """Constants that define defaults."""
 
 EXCLUDE = '.svn,CVS,.bzr,.hg,.git,__pycache__,.tox'
-IGNORE = 'E121,E123,E126,E226,E24,E704'
+IGNORE = 'E121,E123,E126,E226,E24,E704,W503,W504'
 MAX_LINE_LENGTH = 79
 
 # Other consants

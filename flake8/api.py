107f595d13fda6c76ad2ea218359d11209b5d4a4
Ian Cordasco <graffatcolmingov@gmail.com>
2016-06-08 11:48:36 -0500

    Add flake8.api and docs for it
    Stub out the primary method people seem to use.

---
+++ flake8/api.py
@@ -0,0 +1,10 @@
+"""Module containing all public entry-points for Flake8.
+
+This is the only submodule in Flake8 with a guaranteed stable API. All other
+submodules are considered internal only and are subject to change.
+"""
+
+
+def get_style_guide(**kwargs):
+    """Stub out the only function I'm aware of people using."""
+    pass

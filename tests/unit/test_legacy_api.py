8b4f12872e871c287864ee4563c18b4499b8b652
Ian Cordasco <graffatcolmingov@gmail.com>
2016-07-16 21:06:27 -0500

    Add more simple tests for the legacy api

--- tests/unit/test_legacy_api.py
+++ tests/unit/test_legacy_api.py
@@ -20,3 +20,31 @@ def test_get_style_guide():
     mockedapp.make_guide.assert_called_once_with()
     mockedapp.make_file_checker_manager.assert_called_once_with()
     assert isinstance(style_guide, api.StyleGuide)
+
+
+def test_styleguide_options():
+    """Show tha we proxy the StyleGuide.options attribute."""
+    app = mock.Mock()
+    app.options = 'options'
+    style_guide = api.StyleGuide(app)
+    assert style_guide.options == 'options'
+
+
+def test_styleguide_paths():
+    """Show tha we proxy the StyleGuide.paths attribute."""
+    app = mock.Mock()
+    app.paths = 'paths'
+    style_guide = api.StyleGuide(app)
+    assert style_guide.paths == 'paths'
+
+
+def test_styleguide_check_files():
+    """Verify we call the right application methods."""
+    paths = ['foo', 'bar']
+    app = mock.Mock()
+    style_guide = api.StyleGuide(app)
+    report = style_guide.check_files(paths)
+
+    app.run_checks.assert_called_once_with(paths)
+    app.report_errors.assert_called_once_with()
+    assert isinstance(report, api.Report)

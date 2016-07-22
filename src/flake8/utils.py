4a46412bf6007356775657e4f0bc452564dec2d1
Ian Cordasco <graffatcolmingov@gmail.com>
2016-07-22 17:12:49 -0500

    Check for alternate_separator only when truthy
    In the case where alternate separator is None, we use '' which will
    always be in any string. We want to skip that case.
    
    Also we only run our tests on AppVeyor, not all of our testenvs.

--- src/flake8/utils.py
+++ src/flake8/utils.py
@@ -59,7 +59,8 @@ def normalize_path(path, parent=os.curdir):
     separator = os.path.sep
     # NOTE(sigmavirus24): os.path.altsep may be None
     alternate_separator = os.path.altsep or ''
-    if separator in path or alternate_separator in path:
+    if separator in path or (alternate_separator and
+                             alternate_separator in path):
         path = os.path.abspath(os.path.join(parent, path))
     return path.rstrip(separator + alternate_separator)
 

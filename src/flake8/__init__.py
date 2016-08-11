2b77f2d9a0d5eb63edc12dc7b6a470218f3067ec
Ian Cordasco <graffatcolmingov@gmail.com>
2016-08-10 19:14:53 -0500

    Bump version to 3.1.0.dev0

--- src/flake8/__init__.py
+++ src/flake8/__init__.py
@@ -27,7 +27,7 @@ LOG.addHandler(NullHandler())
 # Clean up after LOG config
 del NullHandler
 
-__version__ = '3.0.4'
+__version__ = '3.1.0.dev0'
 __version_info__ = tuple(int(i) for i in __version__.split('.') if i.isdigit())
 
 

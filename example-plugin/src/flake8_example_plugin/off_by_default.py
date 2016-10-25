5f3577fca84f6c45110214880854bc9332f634f0
Ian Cordasco <graffatcolmingov@gmail.com>
2016-10-25 18:21:50 -0500

    Add an example plugin project to source tree

---
+++ example-plugin/src/flake8_example_plugin/off_by_default.py
@@ -0,0 +1,17 @@
+"""Our first example plugin."""
+
+
+class ExampleTwo(object):
+    """Second Example Plugin."""
+    name = 'off-by-default-example-plugin'
+    version = '1.0.0'
+
+    off_by_default = True
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def run(self):
+        """Do nothing."""
+        yield (1, 0, 'X200 The off-by-default plugin was enabled',
+               'OffByDefaultPlugin')

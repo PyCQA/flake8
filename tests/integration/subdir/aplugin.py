423980164b258b2ec77d8d9bfccb9bc00b220e31
Carl Meyer <carl@oddbird.net>
2017-10-23 16:49:09 -0700

    Add paths option in local-plugins config file.

---
+++ tests/integration/subdir/aplugin.py
@@ -0,0 +1,16 @@
+"""Module that is off sys.path by default, for testing local-plugin-paths."""
+
+
+class ExtensionTestPlugin2(object):
+    """Extension test plugin in its own directory."""
+
+    name = 'ExtensionTestPlugin2'
+    version = '1.0.0'
+
+    def __init__(self, tree):
+        """Construct an instance of test plugin."""
+        pass
+
+    def run(self):
+        """Do nothing."""
+        pass

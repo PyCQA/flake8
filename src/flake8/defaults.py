cafe7805147a44959038dfb489a95d157a8e43b9
Jesaja Everling <jeverling@gmail.com>
2018-10-28 20:08:29 +0100

    Clearer docs, NOQA_INLINE_REGEXP comment - fixes #469

--- src/flake8/defaults.py
+++ src/flake8/defaults.py
@@ -30,7 +30,7 @@ NOQA_INLINE_REGEXP = re.compile(
     # ``# noqa: E123,W451,F921``
     # ``# NoQA: E123,W451,F921``
     # ``# NOQA: E123,W451,F921``
-    # We do not care about the ``: `` that follows ``noqa``
+    # We do not want to capture the ``: `` that follows ``noqa``
     # We do not care about the casing of ``noqa``
     # We want a comma-separated list of errors
     r"# noqa(?:: (?P<codes>([A-Z][0-9]+(?:[,\s]+)?)+))?",

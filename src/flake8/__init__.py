e7b8493b5d7af36f6c348566a7c3316b30e5b5ee
Anthony Sottile <asottile@umich.edu>
2019-02-04 08:37:16 -0800

    Release 3.7.5

--- src/flake8/__init__.py
+++ src/flake8/__init__.py
@@ -15,7 +15,7 @@ import sys
 LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.NullHandler())
 
-__version__ = "3.7.4"
+__version__ = "3.7.5"
 __version_info__ = tuple(
     int(i) for i in __version__.split(".") if i.isdigit()
 )

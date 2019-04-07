38ab47702a1cfa451af2e74a95ed145a0f4b2d5a
Anthony Sottile <asottile@umich.edu>
2019-04-07 07:35:46 -0700

    Fix crash when file fails to tokenize but parses

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -85,3 +85,17 @@ Configured `per-file-ignores` setting:
     incorrect/*
     values/*
 '''  # noqa: E501
+
+
+def test_tokenization_error_but_not_syntax_error(tmpdir, capsys):
+    """Test that flake8 does not crash on tokenization errors."""
+    with tmpdir.as_cwd():
+        # this is a crash in the tokenizer, but not in the ast
+        tmpdir.join('t.py').write("b'foo' \\\n")
+
+        app = application.Application()
+        app.run(['t.py'])
+
+    out, err = capsys.readouterr()
+    assert out == 't.py:1:1: E902 TokenError: EOF in multi-line statement\n'
+    assert err == ''

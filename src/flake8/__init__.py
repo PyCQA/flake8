d31c5356bbb0a884555662185697ddc6bb46a44c
Anthony Sottile <asottile@umich.edu>
2021-10-10 19:07:21 -0700

    Release 4.0.0

--- src/flake8/__init__.py
+++ src/flake8/__init__.py
@@ -16,7 +16,7 @@ from typing import Type
 LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.NullHandler())
 
-__version__ = "3.9.2"
+__version__ = "4.0.0"
 __version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
 
 
